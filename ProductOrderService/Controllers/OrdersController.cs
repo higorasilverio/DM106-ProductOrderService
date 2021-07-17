9515bac6ef636b89b8467b48cc9ecea1c33df94c
Higor Silvério <higorasilverio@outlook.com>
2021-07-17 16:48:12 -0300

    Code review performed on OrdersController

--- ProductOrderService/Controllers/OrdersController.cs
+++ ProductOrderService/Controllers/OrdersController.cs
@@ -7,7 +7,6 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
-using Microsoft.Ajax.Utilities;
 using ProductOrderService.br.com.correios.ws;
 using ProductOrderService.CRMClient;
 using ProductOrderService.Data;
@@ -20,6 +19,14 @@ namespace ProductOrderService.Controllers
     public class OrdersController : ApiController
     {
         private ProductOrderServiceContext db = new ProductOrderServiceContext();
+
+        private string wgOrder = "";    //Order weight
+        private decimal lgOrder = 0M;   //Order length
+        private decimal hgOrder = 0M;   //Order height
+        private decimal wdOrder = 0M;   //Order width
+        private decimal dmOrder = 0M;   //Order diameter
+        private decimal pcOrder = 0M;   //Order price
+        private string zipTo = "";      //Order zipcode receiver
 
         // GET: api/Orders
         [Authorize (Roles ="ADMIN")]
@@ -35,24 +42,13 @@ namespace ProductOrderService.Controllers
             Order order = db.Orders.Find(id);
 
             if (order == null)
-            {
-                var resp = new HttpResponseMessage(HttpStatusCode.NotFound)
-                {
-                    Content = new StringContent(string.Format("O pedido com id {0} não pôde ser localizado", id)),
-                    ReasonPhrase = "NotFound"
-                };
-                throw new HttpResponseException(resp);
-            }
-            
+                ShowErrorToUser(HttpStatusCode.NotFound,
+                        string.Format("O pedido com id {0} não pôde ser localizado", id), "NotFound");
+
             if (!User.Identity.Name.Equals(order.username) && !User.IsInRole("ADMIN"))
-            {
-                var resp = new HttpResponseMessage(HttpStatusCode.Forbidden)
-                {
-                    Content = new StringContent("O pedido só pode ser visualizado pelo usuário que o criou ou por um usuário administrador"),
-                    ReasonPhrase = "Forbidden"
-                };
-                throw new HttpResponseException(resp);
-            }
+                ShowErrorToUser(HttpStatusCode.Forbidden,
+                        "O pedido só pode ser visualizado pelo usuário que o criou ou por um usuário administrador", 
+                        "Forbidden");
             
             return Ok(order);
         }
@@ -62,19 +58,12 @@ namespace ProductOrderService.Controllers
         [Route("byusername")]
         [ResponseType(typeof(Order))]
         public IQueryable<Order> GetAllOrderByUsername(string username)
-
         {
             
             if (!User.Identity.Name.Equals(username) && !User.IsInRole("ADMIN"))
-            {
-                var resp = new HttpResponseMessage(HttpStatusCode.Forbidden)
-                {
-                    Content = new StringContent(
-                        "Os pedidos só podem ser visualizados pelo usuário que os criaram ou por um usuário administrador"),
-                    ReasonPhrase = "Forbidden"
-                };
-                throw new HttpResponseException(resp);
-            }
+                ShowErrorToUser(HttpStatusCode.Forbidden,
+                        "Os pedidos só podem ser visualizados pelo usuário que os criou ou por um usuário administrador",
+                        "Forbidden");
             
             return db.Orders.Where(order => order.username == username);
         }
@@ -84,34 +73,16 @@ namespace ProductOrderService.Controllers
         public IHttpActionResult PostOrder(Order order)
         {
             if (!ModelState.IsValid)
-            {
                 return BadRequest(ModelState);
-            }
 
-            if (User.IsInRole("ADMIN")) {
-                if (order.username == null || order.username.Length == 0)
-                {
-                    var resp = new HttpResponseMessage(HttpStatusCode.BadRequest)
-                    {
-                        Content = new StringContent("Não foi fornecido um usuário válido para o inserção do pedido"),
-                        ReasonPhrase = "BadRequest"
-                    };
-                    throw new HttpResponseException(resp);
-                }
-            } else
-            {
-                if (order.username != null && !order.username.Equals(User.Identity.Name))
-                {
-                    var resp = new HttpResponseMessage(HttpStatusCode.BadRequest)
-                    {
-                        Content = new StringContent("Não é possível criar um pedido para outro usuário"),
-                        ReasonPhrase = "BadRequest"
-                    };
-                    throw new HttpResponseException(resp);
-                }
-                order.username = User.Identity.Name;
-            }
+            if (order.username == null)
+                ShowErrorToUser(HttpStatusCode.BadRequest,
+                            "É necessário informar o usuário a.k.a. `username`", "BadRequest");
 
+            if (!User.IsInRole("ADMIN") && !User.Identity.Name.Equals(order.username))
+                ShowErrorToUser(HttpStatusCode.BadRequest,
+                            "Não é possível criar um pedido para outro usuário", "BadRequest");
+            
             order.status = "novo";
             order.pesoTotal = 0M;
             order.precoFrete = 0M;
@@ -119,9 +90,7 @@ namespace ProductOrderService.Controllers
             order.dataPedido = DateTime.Now;
 
             if (!ModelState.IsValid)
-            {
                 return BadRequest(ModelState);
-            }
 
             db.Orders.Add(order);
             db.SaveChanges();
@@ -136,45 +105,21 @@ namespace ProductOrderService.Controllers
             Order order = db.Orders.Find(id);
 
             if (order == null)
-            {
-                var resp = new HttpResponseMessage(HttpStatusCode.NotFound)
-                {
-                    Content = new StringContent(string.Format("O pedido com id {0} não pôde ser localizado", id)),
-                    ReasonPhrase = "NotFound"
-                };
-                throw new HttpResponseException(resp);
-            }
+                ShowErrorToUser(HttpStatusCode.NotFound,
+                            string.Format("O pedido com id {0} não pôde ser localizado", id), "NotFound");
 
             if (!User.Identity.Name.Equals(order.username) && !User.IsInRole("ADMIN"))
-            {
-                var resp = new HttpResponseMessage(HttpStatusCode.Forbidden)
-                {
-                    Content = new StringContent("O pedido só pode ser alterado pelo usuário que o criou ou por um usuário administrador"),
-                    ReasonPhrase = "Forbidden"
-                };
-                throw new HttpResponseException(resp);
-            }
+                ShowErrorToUser(HttpStatusCode.Forbidden,
+                            "O pedido só pode ser alterado pelo usuário que o criou ou por um usuário administrador", 
+                            "Forbidden");
 
             if (order.status.Equals("fechado"))
-            {
-                var resp = new HttpResponseMessage(HttpStatusCode.BadRequest)
-                {
-                    Content = new StringContent("Este pedido já encontra-se fechado"),
-                    ReasonPhrase = "BadRequest"
-                };
-                throw new HttpResponseException(resp);
-            }
+                ShowErrorToUser(HttpStatusCode.BadRequest,
+                        "Este pedido já encontra-se fechado", "BadRequest");
 
             if (order.precoFrete == 0M)
-            {
-                var resp = new HttpResponseMessage(HttpStatusCode.BadRequest)
-                {
-                    Content = new StringContent(
-                        "O status do pedido não permite o seu fechamento. É necessário calcular o frete primeiramente"),
-                    ReasonPhrase = "BadRequest"
-                };
-                throw new HttpResponseException(resp);
-            }
+                ShowErrorToUser(HttpStatusCode.BadRequest,
+                        "Antes de fechar o pedido calcule seu frete `GET: api/Orders/frete?id={id}`", "BadRequest");
 
             order.status = "fechado";
 
@@ -197,25 +142,15 @@ namespace ProductOrderService.Controllers
         public IHttpActionResult DeleteOrder(int id)
         {
             Order order = db.Orders.Find(id);
+
             if (order == null)
-            {
-                var resp = new HttpResponseMessage(HttpStatusCode.NotFound)
-                {
-                    Content = new StringContent(string.Format("O pedido com id {0} não pode ser localizado", id)),
-                    ReasonPhrase = "NotFound"
-                };
-                throw new HttpResponseException(resp);
-            }
-            
+                ShowErrorToUser(HttpStatusCode.NotFound,
+                                string.Format("O pedido com id {0} não pôde ser localizado", id), "NotFound");
+
             if (!User.Identity.Name.Equals(order.username) && !User.IsInRole("ADMIN"))
-            {
-                var resp = new HttpResponseMessage(HttpStatusCode.Forbidden)
-                {
-                    Content = new StringContent("O pedido só pode ser apagado pelo usuário que o criou ou por um usuário administrador"),
-                    ReasonPhrase = "Forbidden"
-                };
-                throw new HttpResponseException(resp);
-            }
+                ShowErrorToUser(HttpStatusCode.Forbidden,
+                            "O pedido só pode ser apagado pelo usuário que o criou ou por um usuário administrador",
+                            "Forbidden");
             
             db.Orders.Remove(order);
             db.SaveChanges();
@@ -232,63 +167,39 @@ namespace ProductOrderService.Controllers
             Order order = db.Orders.Find(id);
 
             if (order == null)
-            {
-                var resp = new HttpResponseMessage(HttpStatusCode.NotFound)
-                {
-                    Content = new StringContent(string.Format("O pedido com id {0} não pode ser localizado", id)),
-                    ReasonPhrase = "NotFound"
-                };
-                throw new HttpResponseException(resp);
-            }
+                ShowErrorToUser(HttpStatusCode.NotFound,
+                                    string.Format("O pedido com id {0} não pôde ser localizado", id), "NotFound");
 
             if (!User.Identity.Name.Equals(order.username) && !User.IsInRole("ADMIN"))
-            {
-                var resp = new HttpResponseMessage(HttpStatusCode.Forbidden)
-                {
-                    Content = new StringContent("O pedido só pode ser consultado pelo usuário que o criou ou por um usuário administrador"),
-                    ReasonPhrase = "Forbidden"
-                };
-                throw new HttpResponseException(resp);
-            }
+                ShowErrorToUser(HttpStatusCode.Forbidden,
+                            "O pedido só pode ser alterado pelo usuário que o criou ou por um usuário administrador",
+                            "Forbidden");
 
-            string cepDestino = this.getCEP(order);
+            if (order.OrderItems.Count == 0)
+                ShowErrorToUser(HttpStatusCode.BadRequest,
+                        "O pedido não possui produtos associados", "BadRequest");
+
+            zipTo = GetZip(order);
+            if (zipTo == null)
+                ShowErrorToUser(HttpStatusCode.BadRequest,
+                        "Não foi possível obter o CEP do usuário (Erro no serviço de CRM)", "BadRequest");
 
             OrderItem[] itemArray = new OrderItem[order.OrderItems.Count];
             order.OrderItems.CopyTo(itemArray, 0);
-
-            if (itemArray.Length == 0)
-            {
-                var resp = new HttpResponseMessage(HttpStatusCode.BadRequest)
-                {
-                    Content = new StringContent("O pedido não possui produtos associados"),
-                    ReasonPhrase = "BadRequest"
-                };
-                throw new HttpResponseException(resp);
-            }
-
-            string pesoPedido = CalculaPeso(itemArray);
-
-            decimal comprimentoPedido = CalculaComprimento(itemArray);
-
-            decimal alturaPedido = CalculaAltura(itemArray);
-
-            decimal larguraPedido = CalculaLargura(itemArray);
-
-            decimal diametroPedido = CalculaDiametro(itemArray);
-
-            decimal precoPedido = CalculaPreco(itemArray);
+            GetOrderData(itemArray);
 
             CalcPrecoPrazoWS correios = new CalcPrecoPrazoWS();
             cResultado resultado = correios.CalcPrecoPrazo(
-                "", "", "40010", "05428-000", cepDestino, pesoPedido, 1, 
-                comprimentoPedido, alturaPedido, larguraPedido, diametroPedido, "N", precoPedido, "S");
+                "", "", "40010", "05428-000", zipTo, wgOrder, 1, lgOrder, hgOrder, wdOrder, dmOrder, "N", pcOrder, "S");
+
             if (resultado.Servicos[0].Erro.Equals("0"))
             {
-                order.precoFrete = Decimal.Parse(resultado.Servicos[0].Valor);
+                order.precoFrete = decimal.Parse(resultado.Servicos[0].Valor);
                 order.dataEntrega = DateTime.Now.AddDays(double.Parse(resultado.Servicos[0].PrazoEntrega));
-                order.pesoTotal = Convert.ToDecimal(pesoPedido);
-                order.precoTotal = precoPedido;
-                
+                order.pesoTotal = Convert.ToDecimal(wgOrder);
+                order.precoTotal = pcOrder;
+                ClearOrderData();
+
                 db.Entry(order).State = EntityState.Modified;
 
                 try
@@ -304,121 +215,83 @@ namespace ProductOrderService.Controllers
             }
             else
             {
-                var resp = new HttpResponseMessage(HttpStatusCode.BadRequest)
-                {
-                    Content = new StringContent(resultado.Servicos[0].MsgErro),
-                    ReasonPhrase = "BadRequest"
-                };
-                throw new HttpResponseException(resp);
+                ClearOrderData();
+                return BadRequest(resultado.Servicos[0].MsgErro);
             }
+                
         }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
-            {
                 db.Dispose();
-            }
+
             base.Dispose(disposing);
         }
 
-        private string getCEP(Order order)
+        private string GetZip(Order order)
         {
-            string user;
+            string user = User.IsInRole("User") ? User.Identity.Name : order.username;
 
-            if (order == null)
-            {
-                var resp = new HttpResponseMessage(HttpStatusCode.BadRequest)
-                {
-                    Content = new StringContent("Não foi fornecido o pedido"),
-                    ReasonPhrase = "BadRequest"
-                };
-                throw new HttpResponseException(resp);
-            }
-
-            if (User.IsInRole("User"))
-                user = User.Identity.Name;
-            else
-                user = order.username;
             CRMRestClient crmClient = new CRMRestClient();
             Customer customer = crmClient.GetCustomerByEmail(user);
 
             if (customer != null)
-            {
                 return customer.zip;
-            }
-
             else
-            {
-                var resp = new HttpResponseMessage(HttpStatusCode.BadRequest)
-                {
-                    Content = new StringContent("Não foi possível obter o CEP do usuário"),
-                    ReasonPhrase = "BadRequest"
-                };
-                throw new HttpResponseException(resp);
-            }
+                return null;
         }
 
-        private decimal CalculaPreco(OrderItem[] itemArray)
+        private void GetOrderData(OrderItem[] itemArray)
         {
+            pcOrder = 0M;
+            for (int i = 0; i < itemArray.Length; i++)
+                pcOrder += itemArray[i].Product.preco * itemArray[i].quantidade;
+
+            dmOrder = 0M;
+            for (int i = 0; i < itemArray.Length; i++)
+                dmOrder += itemArray[i].Product.diametro * itemArray[i].quantidade;
+
+            hgOrder = 0M;
+            for (int i = 0; i < itemArray.Length; i++)
+                hgOrder += itemArray[i].Product.altura * itemArray[i].quantidade;
+
+            wdOrder = 0M;
+            for (int i = 0; i < itemArray.Length; i++)
+                if (itemArray[i].Product.largura > wdOrder)
+                    wdOrder = itemArray[i].Product.largura;
+
+            lgOrder = 0M;
+            for (int i = 0; i < itemArray.Length; i++)
+                if (itemArray[i].Product.comprimento > lgOrder)
+                    lgOrder = itemArray[i].Product.largura;
+
             decimal sum = 0M;
+            wgOrder = "";
             for (int i = 0; i < itemArray.Length; i++)
-            {
-                sum += itemArray[i].Product.preco * itemArray[i].quantidade;
-            }
-            return sum;
-        }
-
-        private decimal CalculaDiametro(OrderItem[] itemArray)
-        {
-            decimal sum = 0M;
-            for (int i = 0; i < itemArray.Length; i++)
-            {
-                sum += itemArray[i].Product.diametro * itemArray[i].quantidade;
-            }
-            return sum;
-        }
-
-        private decimal CalculaAltura(OrderItem[] itemArray)
-        {
-            decimal sum = 0M;
-            for (int i = 0; i < itemArray.Length; i++)
-            {
-                sum += itemArray[i].Product.altura * itemArray[i].quantidade;
-            }
-            return sum;
-        }
-
-        private decimal CalculaLargura(OrderItem[] itemArray)
-        {
-            decimal largura = 0M;
-            for (int i = 0; i < itemArray.Length; i++)
-            {
-                if (itemArray[i].Product.largura > largura)
-                    largura = itemArray[i].Product.largura;
-            }
-            return largura;
-        }
-        
-        private decimal CalculaComprimento(OrderItem[] itemArray)
-        {
-            decimal comprimento = 0M;
-            for (int i = 0; i < itemArray.Length; i++)
-            {
-                if (itemArray[i].Product.comprimento > comprimento)
-                    comprimento = itemArray[i].Product.largura;
-            }
-            return comprimento;
-        }
-
-        private string CalculaPeso(OrderItem[] itemArray)
-        {
-            decimal sum = 0M;
-            for (int i = 0; i < itemArray.Length; i++)
-            {
                 sum += itemArray[i].Product.peso * itemArray[i].quantidade;
-            }
-            return sum.ToString("F2");
+            wgOrder = sum.ToString("F2");
+        }
+
+        private void ClearOrderData()
+        {
+            wgOrder = "";
+            lgOrder = 0M;
+            hgOrder = 0M;
+            wdOrder = 0M;
+            dmOrder = 0M;
+            pcOrder = 0M;
+            zipTo = "";
+        }
+
+        private void ShowErrorToUser(HttpStatusCode statusCode, string content, string reasonPhrase)
+        {
+            var resp = new HttpResponseMessage(statusCode)
+            {
+                Content = new StringContent(content),
+                ReasonPhrase = reasonPhrase
+            };
+            throw new HttpResponseException(resp);
         }
     }
 }
